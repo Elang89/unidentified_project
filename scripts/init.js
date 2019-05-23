50ca4693f9d5ee0c0c6412ac60659f5bdeb8482a
Ernesto Lang <elang8899@gmail.com>
2019-05-22 23:06:47 -0600

    changed mongo init.js script

--- scripts/init.js
+++ scripts/init.js
@@ -1,6 +1,10 @@
 conn = new Mongo();
 db = conn.getDB("admin");
 db.auth("admin", "password");
+
+db = db.getSiblingDB("plpdb");
+db.auth("user", "password");
+
 db.createUser(
     {
         user: "user",
@@ -9,6 +13,3 @@ db.createUser(
     }
 );
 
-db = db.getSiblingDB("plpdb");
-db.auth("user", "password")
-db.subscribers.insertOne({name: "Dummy"});
