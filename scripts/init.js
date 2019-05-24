0085426c0117fe3f766545d7d0d334fb30e41413
Ernesto Lang <elang8899@gmail.com>
2019-05-24 17:24:02 -0600

    Switched MongoClientModule to MongoDatabaseModule

--- scripts/init.js
+++ scripts/init.js
@@ -1,15 +1,14 @@
+
 conn = new Mongo();
-db = conn.getDB("admin");
-db.auth("admin", "password");
 
-db = db.getSiblingDB("plpdb");
-db.auth("user", "password");
+db = conn.getDB('admin');
 
-db.createUser(
-    {
-        user: "user",
-        pwd: "password",
-        roles: [ {role: "readWrite", db: "plpdb"}]
-    }
-);
+db.auth('root', 'example');
 
+db = db.getSiblingDB('pixeldb');
+
+db.createUser({
+    user: 'user',
+    pwd: 'password',
+    roles: [{ role: 'readWrite', db: 'pixeldb' }],
+});
