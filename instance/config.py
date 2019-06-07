a57a81be8915cbd22be459032d850cd006f20b30
Elang89 <elang8899@gmail.com>
2019-06-07 12:53:40 -0600

    Merging all changes into master (#10)
    * Feature/plp route setup (#1)
    
    * Added blueprint
    
    * Added database, model for subscribers and changed deps
    
    * created routes for project
    
    * changed mongo init.js script
    
    * Switched MongoClientModule to MongoDatabaseModule (#2)
    
    * Feature/plp switch to mongoengine (#3)
    
    * Added mongoengine
    
    * switched to pymongo in providers
    
    * Feature/plp switch to pgsql (#4)
    
    * changed docker from mongo to postgres
    
    * added init.py files
    
    * fully moved to pgsql
    
    * added flask migrate
    
    * added migrations to env, succesfully migrated first table
    
    * added circle ci and small test to see if it works (#5)
    
    * added circle ci and small test to see if it works
    
    * fixed issue with environments in circle ci config yml
    
    * fixed issue with environments in circle ci config yml
    
    * fixed issue with environments in circle ci config yml
    
    * pipenv run instead of shell
    
    * deleted several files, added images for nginx and python
    
    * added proper dependency command circleci, modified sites.conf
    
    * changed command for pytest in circle ci again
    
    * yet another attempt at fixing pytest with circle ci
    
    * yet another attempt at fixing pytest with circle ci
    
    * yet another attempt at fixing pytest with circle ci
    
    * yet another attempt at fixing pytest with circle ci
    
    * yet another attempt at fixing pytest with circle ci
    
    * yet another attempt at fixing pytest with circle ci
    
    * yet another attempt at fixing pytest with circle ci
    
    * yet another attempt at fixing pytest with circle ci
    
    * yet another attempt at fixing pytest with circle ci
    
    * Changed config back to how it was
    
    * Changed Pipfile locations
    
    * changed project structure, added dockerignore, changed circle ci config, removed pipenv added poetry, removed psycopg2 and added psycopg2-binary (#6)
    
    * Feature/plp switch to requirements (#7)
    
    * added requirements-dev and requirements
    
    * changed circleci config to adapt to new changes
    
    * removed open ports in compose file
    
    * removed poetry from compose file
    
    * added gunicorn and fixed circle ci build
    
    * removed FLASK_APP environment variable
    
    * added virtual env to ignore
    
    * fixed conflict
    
    * Feature/up gunicorn setup (#8)
    
    * set up gunicorn
    
    * set up gunicorn
    
    * modified alembic ini to connect to db, and scripts (#9)

--- instance/config.py
+++ instance/config.py
@@ -1,4 +1,14 @@
-import os
+from os import getenv
+
+
+def _create_connection_string(user, password, host, port, db):
+    return 'postgresql://{user}:{password}@{host}:{port}/{db}'.format(
+        user=user,
+        password=password,
+        host=host,
+        port=port,
+        db=db
+    )
 
 
 class Config(object):
@@ -6,14 +16,21 @@ class Config(object):
     Arguments:
         object {object} -- default python object type
     """
-    DEBUG = False
-    SECRET = os.getenv('SECRET')
-    MONGO_DATABASE_URI = os.getenv('DATABASE_URL')
+
+    DEBUG = True
+    PG_HOSTNAME = getenv('PG_HOSTNAME')
+    PG_USER = getenv('PG_USER')
+    PG_PASSWORD = getenv('PG_PASSWORD')
+    PG_PORT = getenv('PG_PORT')
+    PG_DATABASE = getenv('PG_DATABASE')
+
+    SQLALCHEMY_DATABASE_URI = _create_connection_string(
+        user=PG_USER, password=PG_PASSWORD, host=PG_HOSTNAME, port=PG_PORT, db=PG_DATABASE)
+    
 
 
 class DevelopmentConfig(Config):
     """Development config settings
-    
     Arguments:
         Config {Config} -- default config object from 
         which class inherits.
@@ -23,33 +40,32 @@ class DevelopmentConfig(Config):
 
 class TestingConfig(Config):
     """Testing config settings
-    
     Arguments:
         Config {Config} -- default config object from
         which class inherits.
     """
+
     TESTING = True
-    MONGO_DATABASE_URI = 'postgresql://localhost/test_db'
     DEBUG = True
 
 
 class StagingConfig(Config):
     """Staging config settings
-    
     Arguments:
         Config {Config} -- default config object from
         which class inherits.
     """
+
     DEBUG = True
 
 
 class ProductionConfig(Config):
     """Production config settings
-    
     Arguments:
         Config {Config} -- default config object from 
         which class inherits.
     """
+
     DEBUG = False
     TESTING = False
 
