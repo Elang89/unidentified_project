b7f04a5e5ddf20b78e7fafd9f9a9fd6deb0c6a61
Elang87 <ernesto@wondersauce.com>
2019-06-07 12:05:56 -0600

    modified alembic ini to connect to db, and scripts

--- dockerfile
+++ dockerfile
@@ -4,11 +4,22 @@ RUN mkdir flask_app
 
 WORKDIR /flask_app
 
-COPY requirements-dev.txt requirements-dev.txt
-COPY requirements.txt requirements.txt
+COPY . .
 
 RUN python -m venv env
 RUN . env/bin/activate
 
 RUN pip install -r requirements.txt
 RUN pip install -r requirements-dev.txt
+
+ENV ENV_PORT 5000
+
+ENV FLASK_ENV staging
+
+ENV PG_HOSTNAME localhost
+ENV PG_PORT 5432
+ENV PG_USER root
+ENV PG_PASSWORD password
+ENV PG_DATABASE structure_db
+
+RUN alembic upgrade head
---
+++ scripts/versions/2eff00653f73_create_subscribers_table.py
@@ -0,0 +1,32 @@
+"""create_subscribers_table
+
+Revision ID: 2eff00653f73
+Revises: 
+Create Date: 2019-06-07 09:37:36.118997
+
+"""
+from alembic import op
+from uuid import uuid4
+from sqlalchemy.dialects.postgresql import UUID
+
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2eff00653f73'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        'subscribers',
+        sa.Column('id', UUID(as_uuid=True), primary_key=True, default=uuid4),
+        sa.Column('created_at', sa.DateTime, default=sa.func.now()),
+        sa.Column('updated_at', sa.DateTime, nullable=True)
+    )
+
+
+def downgrade():
+    op.drop_table('subscribers')
